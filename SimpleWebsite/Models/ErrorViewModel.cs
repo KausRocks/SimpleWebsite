cb135522734ba4c4bdbdbcbcc7f0b6662ad35f71
Kaustav Halder <>
2022-06-05 17:13:50 +0000

    First Commit

---
+++ SimpleWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
